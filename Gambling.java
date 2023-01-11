b9d691e841ebee5348206ab0b115aaa6f139c8c1
Abhishek Kanojia <abhishek14kanojia@gmail.com>
2023-01-11 14:04:58 +0530

    UC4

--- Gambling.java
+++ Gambling.java
@@ -3,32 +3,50 @@ package com.workShop;
 import java.util.Scanner;
 
 public class Gambling {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("enter the amount of stack");
-		int Stake = sc.nextInt();
-		System.out.println("enter the price of game");
-		int betPrice = sc.nextInt();
-		 int maxStake = Stake + 50 % Stake;
-	     int minStake = Stake - 50 % Stake;
-		
-		System.out.println(Stake);
-		System.out.println(betPrice);
-		 while (Stake > minStake && Stake < maxStake) {
-		int play = (int)(Math.random()*2);
-		
-		switch(play) {
-		case 0:
-			Stake = Stake + betPrice;
-			System.out.println(Stake);
-		case 1:
-			Stake = Stake - betPrice;
-			System.out.println(Stake);
-		}
-		
-		}
-		 System.out.println("50% stake limit :" + Stake);
-		
+	 int stakePerDay;
+	    int betPrice = 1;
+	    int minStake = 50;
+	    int maxStake = 150;
+	    int win = 0;
+	    int loose = 0;
+	    int profit = 0;
+
+	    public void play() {
+	        while (stakePerDay > minStake && stakePerDay < maxStake) 
+	        {
+	            int play = (int) (Math.random() * 2); 
+	            switch (play) {
+	                case 0: 
+	                    stakePerDay += betPrice;
+	                    break;
+
+	                case 1: 
+	                    stakePerDay -= betPrice;
+	                    break;
+	            }
+	        }
+	    }
+
+	    public void gamble20Days() {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.println("Please enter the amount of Stake:");
+	        int num = scanner.nextInt();
+	        for (int days = 1; days <= 20; days++) {
+	            stakePerDay = num;
+	            play();
+	            System.out.println(" \n The player has stake of " + stakePerDay + " on day " + days);
+	            int balanceDay = stakePerDay - 100; 
+	            profit += balanceDay;
+	            if (stakePerDay > minStake) {
+	                win++; 
+	            } else {
+	                loose++;
+	            }
+	            System.out.println("The total profit or loss of the day is " + balanceDay);
+	        }
+	        System.out.println(" \n In total 20 days, the player has won " + win + " days and loose " + loose + " days");
+	        System.out.println("The total gain amount for 20 Days is $" + profit);
+	        scanner.close();
 		
 		}
 
