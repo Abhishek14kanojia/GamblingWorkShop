88d2bcd64952eb677be27fe419ab244037ed982e
Abhishek Kanojia <abhishek14kanojia@gmail.com>
2023-01-11 13:58:04 +0530

    UC1

---
+++ Gambling.java
@@ -0,0 +1,18 @@
+package com.workShop;
+
+import java.util.Scanner;
+
+public class Gambling {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the amount of stack");
+		int Stack = sc.nextInt();
+		System.out.println("enter the price of game");
+		int betPrice = sc.nextInt();
+		
+		System.out.println(Stack);
+		System.out.println(betPrice);
+		
+		}
+
+}
